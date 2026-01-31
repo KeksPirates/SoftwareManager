89de75c3d792c4d3ea4c7de67001faaefde7f74e
Vxrtrauter <101264710+Vxrtrauter@users.noreply.github.com>
2026-01-31 04:52:38 +0100

    link update_settings to settings dialog

--- core/utils/config/settings.py
+++ core/utils/config/settings.py
@@ -1,3 +1,4 @@
+from core.network.libtorrent_int import update_settings
 from core.utils.data.state import state
 from .config import create_config
 
@@ -20,5 +21,6 @@ def save_settings(close=lambda: None, apiurl=None, download_path=None, down_spee
     if max_downloads is not None:
         state.max_downloads = max_downloads
     
+    update_settings()
     create_config()
     close()
