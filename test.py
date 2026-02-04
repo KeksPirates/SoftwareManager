f2a79e5eed0f6580cf76d1ddd492d7ddae310373
Vxrtrauter <101264710+Vxrtrauter@users.noreply.github.com>
2026-02-04 04:04:31 +0100

    test more stuff

--- test.py
+++ test.py
@@ -3,9 +3,26 @@ import psutil
 addrs = psutil.net_if_addrs()
 stats = psutil.net_if_stats()
 
-for interface in addrs.keys():
-    print(interface)
+for interface, addr_list in addrs.items():
+    up = stats[interface].isup
+    for addr in addr_list:
+        if addr.family == 2: # ipv4
+            ipv4 = addr.address
+            if not ipv4.startswith("127.") and not ipv4.startswith("169.254"):
+                print(f'"{interface}": True')
 
 print(addrs)
 print()
-print(stats)
+sigmer = list()
+for interface in stats.keys():
+    print(f"{interface}: UP = {stats[interface].isup}")
+    sigmer.append(f"{interface}: {stats[interface].isup}")
+
+
+
+
+
+
+
+
+print(sigmer)
