11b8f3f2654be6b2fbfaa21100dcf135b53b0b3c
shayaa <101264710+Vxrtrauter@users.noreply.github.com>
2026-03-10 00:21:57 +0100

    Update update_checker.py

--- src/core/utils/network/update_checker.py
+++ src/core/utils/network/update_checker.py
@@ -4,11 +4,16 @@ from core.utils.logging.logs import consoleLog
 
 def get_updates():
     url = f"https://api.github.com/repos/KeksPirates/SoftwareManager/releases/latest"
-    response = requests.get(url, timeout=15)
+
+    try:
+        response = requests.get(url, timeout=15)
+    except requests.RequestException as e:
+        consoleLog(f"Failed to fetch releases: {e}")
+        return None
 
     if response.status_code != 200:
         consoleLog(f"Failed to fetch releases: {response.status_code}")
-        return None, None
+        return None
 
     release = response.json()
 
@@ -29,8 +34,8 @@ def get_updates():
                     hash=asset['digest']
                 ))
             return release_assets
-        else: 
-            return None, None
+        else:
+            return None
     else:
         consoleLog("Already up-to-date.")
-        return None, None
+        return None
