0e05f427419c7a2a00eb5787869972e01c6d20b3
Vxrtrauter <101264710+Vxrtrauter@users.noreply.github.com>
2026-03-16 01:35:52 +0100

    Fix invalid version detection, refactor main.py and add comments for improved code understanding

--- src/core/interface/dialogs/update.py
+++ src/core/interface/dialogs/update.py
@@ -1,18 +1,22 @@
 from core.utils.network.update_checker import get_updates
 from core.utils.network.updater import download_update
 from PySide6.QtWidgets import QDialog, QMessageBox
+from core.utils.logging.logs import consoleLog
 from core.utils.data.state import state
+from pathlib import Path
 import platform
 import json
 import os
 
 def check_version():
-    build_info_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "build_info.json")
+    # Get build info filepath
+    build_info_path = Path(__file__).resolve().parents[3] / "build_info.json" # parents[3] = climb up 4 directories from the file ran
     if os.path.exists(build_info_path):
         with open(build_info_path, "r") as f:
             build_info = json.load(f)
             state.version = build_info.get("version")
-
+    else:
+        consoleLog(f"Could not find build info file (Path: {build_info_path})")
 
 class UpdateDialog(QDialog):
     def __init__(self, parent=None):
--- src/core/utils/general/wrappers.py
+++ src/core/utils/general/wrappers.py
@@ -1,2 +1,9 @@
+from core.utils.logging.logs import consoleLog
+
 def run_thread(thread):
-    thread.start()
+    target_name = thread._target.__name__
+    try:
+        thread.start()
+        consoleLog(f"Started Thread: {target_name}")
+    except Exception as e:
+        consoleLog(f"Exception while starting thread {target_name}: {e}")
